7cabd8eab37a28ad26322c2e55c6aaf7289cef86
riahim@lsbu.ac.uk <>
2014-03-29 22:33:46 +0000

    fixed routes for controller

--- module/Users/config/module.config.php
+++ module/Users/config/module.config.php
@@ -5,6 +5,26 @@ return array(
             'Users\Controller\User' => 'Users\Controller\UserController',
         ),
     ),
+    
+    // The following section is new and should be added to your file
+    'router' => array(
+        'routes' => array(
+            'users' => array(
+                'type'    => 'segment',
+                'options' => array(
+                    'route'    => '/users[/:action][/:id]',
+                    'constraints' => array(
+                        'action' => '[a-zA-Z][a-zA-Z0-9_-]*',
+                        'id'     => '[0-9]+',
+                    ),
+                    'defaults' => array(
+                        'controller' => 'Users\Controller\User',
+                        'action'     => 'index',
+                    ),
+                ),
+            ),
+        ),
+    ),
     'view_manager' => array(
         'template_path_stack' => array(
             'users' => __DIR__ . '/../view',
