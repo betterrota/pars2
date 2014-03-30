4b47ddd2ffd7b0ba1a13da629e063396fd1e8c9d
riahim@lsbu.ac.uk <>
2014-03-30 20:59:46 +0000

    commiting new module users redonen

--- module/Users/config/module.config.php
+++ module/Users/config/module.config.php
@@ -1,39 +1,35 @@
-
 <?php
 return array(
-'controllers' => array(
-'invokables' => array(
-'Users\Controller\Index' =>
-'Users\Controller\IndexController',
-),
-),
+    'controllers' => array(
+        'invokables' => array(
+           'Users\Controller\Index' =>'Users\Controller\IndexController',
 
-'router' => array(
+        ),
+    ),
+    'router' => array(
 'routes' => array(
 'users' => array(
 'type' => 'Literal',
 'options' => array(
 'route' => '/users',
 'defaults' => array(
-'__NAMESPACE__' =>
-'Users\Controller',
+'__NAMESPACE__' =>'Users\Controller',
 'controller' => 'Index',
 'action' => 'index',
-),
+,
 ),
 'may_terminate' => true,
 'child_routes' => array(
+
 'default' => array(
 'type' => 'Segment',
-'options' => array('/[:controller[/:action]]',
+'options' => array(
+'route' =>'/[:controller[/:action]]',
 'constraints' => array(
-'controller' => 'Users\Controller\Index',
-                        'action'     => 'index',
-
+'controller' =>'[a-zA-Z][a-zA-Z0-9_-]*',
+'action' =>'[a-zA-Z][a-zA-Z0-9_-]*',
 ),
-    'defaults' => array(
-  
-
+'defaults' => array(
 ),
 ),
 ),
@@ -43,9 +39,9 @@ return array(
 ),
 
 
-'view_manager' => array(
-'template_path_stack' => array(
-'users' => __DIR__ . '/../view',
-),
-),
+    'view_manager' => array(
+        'template_path_stack' => array(
+            'users' => __DIR__ . '/../view',
+        ),
+    ),
 );
--- module/Users/src/Users/Controller/IndexController.php
+++ module/Users/src/Users/Controller/IndexController.php
@@ -1,24 +1,24 @@
 <?php
 namespace Users\Controller;
+
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-class IndexController extends AbstractActionController
+
+class Userontroller extends AbstractActionController
 {
-public function indexAction()
-{
-$view = new ViewModel();
-return $view;
-}
-public function registerAction()
-{
-$view = new ViewModel();
-$view->setTemplate('users/index/new-user');
-return $view;
-}
-public function loginAction()
-{
-$view = new ViewModel();
-$view->setTemplate('users/index/login');
-return $view;
-}
+    public function indexAction()
+    {
+    }
+
+    public function addAction()
+    {
+    }
+
+    public function editAction()
+    {
+    }
+
+    public function deleteAction()
+    {
+    }
 }
