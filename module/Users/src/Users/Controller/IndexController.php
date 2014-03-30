8240d16d6316259b69f85c2e507978aa41c56adb
riahim@lsbu.ac.uk <>
2014-03-30 22:08:50 +0000

    commit useers

--- module/Users/src/Users/Controller/IndexController.php
+++ module/Users/src/Users/Controller/IndexController.php
@@ -1,14 +1,13 @@
 <?php
 namespace Users\Controller;
-
 use Zend\Mvc\Controller\AbstractActionController;
 use Zend\View\Model\ViewModel;
-
 class IndexController extends AbstractActionController
 {
-    public function indexAction()
-    {
-   $view = new ViewModel();
+public function indexAction()
+{
+
+$view = new ViewModel();
 return $view;
 }
 public function registerAction()
@@ -23,6 +22,4 @@ $view = new ViewModel();
 $view->setTemplate('users/index/login');
 return $view;
 }
-}
-
 }
